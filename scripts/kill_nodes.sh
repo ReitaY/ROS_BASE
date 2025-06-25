d790e4ccc18c3cdefe1dd8f2bb77f61347cd5446
ReitaY <raitayamagishi@gmail.com>
2025-06-25 17:41:10 +0900

    add kill gazebo tasks feature

--- scripts/kill_nodes.sh
+++ scripts/kill_nodes.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-echo "Kill all ros nodes!"
-ps aux | grep ros | grep -v grep | awk '{ print "kill -9", $2 }' | sh
+echo "Kill all ros nodes!" 
+ps aux | grep ros | grep -v grep | awk '{print $2}' | xargs sudo kill -9
+killall -9 gzserver gzclient
 
 
