39152c99a01275906233ab816e6855b586b3930f
ReitaY <raitayamagishi@gmail.com>
2025-06-25 17:41:58 +0900

    remove  unnesessary build feature

--- scripts/clean_build_pkgs.sh
+++ scripts/clean_build_pkgs.sh
@@ -4,10 +4,6 @@ SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # リポジトリの絶対パスを取得
 WS_DIR="$(cd "$(dirname $SCRIPT_DIR)" && pwd)"
 cd $WS_DIR/ros_ws
-rm -rf build install log  && \
-colcon build --packages-select vehicle_interface && \
-source install/setup.bash && \
-colcon build --packages-select nedo_localization && \
-source install/setup.bash && \
+rm -rf build install log
 colcon build 
 cd $WS_DIR/scripts
